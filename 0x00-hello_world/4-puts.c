f27b9946237b3ff8e5b482d37a7bea90f5c2cd2a
Hopewell <hopewellnnaji@gmail.com>
2022-09-16 14:04:16 -0700

    Hopewell

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 
 /**
-*	main - Printing
-*
-*	Return: Always 0 (Successful)
-*/ int
-main(void)
+ * main - This uses program prints exactly the string
+ * "Programming is like building a multilingual puzzle
+ * followed by a new line.
+ * Return: always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-       	return (0);
+
+	return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
